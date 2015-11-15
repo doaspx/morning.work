24413c1502a6dd3f5d8934d8ee1b0d5d92e63bfe
Zongmin Lei <leizongmin@gmail.com>
2015-11-15 11:45:03 +0800

    修正在线预览程序

--- build/index.js
+++ build/index.js
@@ -25,7 +25,6 @@ var TARGET_DIR = path.resolve(__dirname, '../page');
 var TPL_LIST = tinyliquid.parse(fs.readFileSync(path.resolve(__dirname, 'tpl_list.html')).toString());
 var TPL_ITEM = tinyliquid.parse(fs.readFileSync(path.resolve(__dirname, 'tpl_item.html')).toString());
 
-
 function readFile (f) {
   var data = fs.readFileSync(f).toString().replace(/\r/g, '');
   var i = data.indexOf('\n\n');
@@ -85,8 +84,9 @@ function getPostList () {
   });
 }
 
-function renderPostList (list, callback) {
+function renderPostList (list, callback, TPL_LIST) {
   list = list || getPostList();
+  var TPL_LIST = TPL_LIST || tinyliquid.parse(fs.readFileSync(path.resolve(__dirname, 'tpl_list.html')).toString());
   var context = tinyliquid.newContext({locals: {list: list}});
   tinyliquid.run(TPL_LIST, context, function (err) {
     if (err) return callback(err);
@@ -97,16 +97,18 @@ function renderPostList (list, callback) {
     writeFile(f, html);
 
     var f2 = path.resolve(__dirname, '../index.html');
+    console.log('write to file: %s', f2);
     writeFile(f2, html);
 
     callback();
   });
 }
 
-function renderPost (item, callback) {
+function renderPost (item, callback, TPL_ITEM) {
   if (typeof item === 'string') {
     item = readFile(item);
   }
+  var TPL_ITEM = TPL_ITEM || tinyliquid.parse(fs.readFileSync(path.resolve(__dirname, 'tpl_item.html')).toString());
   var context = tinyliquid.newContext({locals: item});
   tinyliquid.run(TPL_ITEM, context, function (err) {
     if (!err) {
@@ -134,14 +136,18 @@ function startBuild () {
   console.log('================================================================================');
   console.log('total %s', list.length);
 
-  async.eachSeries(list, renderPost, function (err) {
+  async.eachSeries(list, function (item, next) {
+
+    renderPost(item, next, TPL_ITEM);
+
+  }, function (err) {
     if (err) throw err;
 
     renderPostList(list, function (err) {
       if (err) throw err;
 
       console.log('done');
-    });
+    }, TPL_LIST);
   });
 }
 
